05fea6e4f08afd15ff0d8b49e07632da34c77b49
SenselessObsessed <toni.pakhomov.2015@mail.ru>
2024-09-06 21:45:42 +0400

    initial commit

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,4 @@
+test("should be a true random num", () => {
+  const num = 2;
+  expect(num < 21).toBeTruthy();
+});
---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "production",
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+});
